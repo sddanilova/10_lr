b231f70ad6f475450dbed7d8cec3ea783595ebc6
sddanilova <62462660+sddanilova@users.noreply.github.com>
2020-05-31 00:12:56 +0300

    Update 10_lr.cpp

--- 10_lr/10_lr/10_lr.cpp
+++ 10_lr/10_lr/10_lr.cpp
@@ -1,4 +1,4 @@
-// 10_lr.cpp: определяет точку входа для консольного приложения.
+// 10_lr.cpp: Г®ГЇГ°ГҐГ¤ГҐГ«ГїГҐГІ ГІГ®Г·ГЄГі ГўГµГ®Г¤Г  Г¤Г«Гї ГЄГ®Г­Г±Г®Г«ГјГ­Г®ГЈГ® ГЇГ°ГЁГ«Г®Г¦ГҐГ­ГЁГї.
 //
 
 #include "stdafx.h"
@@ -9,9 +9,9 @@
 
 using namespace std;
 
-class APDExc { //класс исключений
+class APDExc { //РєР»Р°СЃСЃ РёСЃРєР»СЋС‡РµРЅРёР№
 public:
-	class BelowZero { // отрицательное число
+	class BelowZero { // РѕС‚СЂРёС†Р°С‚РµР»СЊРЅРѕРµ С‡РёСЃР»Рѕ
 	public: BelowZero(string asd) {
 		error = asd;
 	}
@@ -46,13 +46,13 @@ class ApplicationDevelopmentSetter {
 template <typename a, typename b>
 class ApplicationDevelopment : ApplicationDevelopmentGetter<a, b>, ApplicationDevelopmentSetter<a, b> {
 public:
-	ApplicationDevelopment(); // Конструктор по умолчанию
-	ApplicationDevelopment(const a * nameVal, const a * versionVal, b costVal, const a * platformVal, const a * languageVal); // Конструктор со всеми параметрами
-	ApplicationDevelopment<a, b>(const ApplicationDevelopment<a, b> &val); // Копирующий конструктор
-	~ApplicationDevelopment(); // Деструктор
-	ApplicationDevelopment operator +(ApplicationDevelopment<a, b> val); // Функциия суммы (перегрузка оператора +)
-	void operator =(ApplicationDevelopment<a, b> &val); // Функция присваивания (перегрузка оператора =)
-	void print(); // Функция вывода информации об объекте
+	ApplicationDevelopment(); // РљРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ
+	ApplicationDevelopment(const a * nameVal, const a * versionVal, b costVal, const a * platformVal, const a * languageVal); // РљРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ СЃРѕ РІСЃРµРјРё РїР°СЂР°РјРµС‚СЂР°РјРё
+	ApplicationDevelopment<a, b>(const ApplicationDevelopment<a, b> &val); // РљРѕРїРёСЂСѓСЋС‰РёР№ РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ
+	~ApplicationDevelopment(); // Р”РµСЃС‚СЂСѓРєС‚РѕСЂ
+	ApplicationDevelopment operator +(ApplicationDevelopment<a, b> val); // Р¤СѓРЅРєС†РёРёСЏ СЃСѓРјРјС‹ (РїРµСЂРµРіСЂСѓР·РєР° РѕРїРµСЂР°С‚РѕСЂР° +)
+	void operator =(ApplicationDevelopment<a, b> &val); // Р¤СѓРЅРєС†РёСЏ РїСЂРёСЃРІР°РёРІР°РЅРёСЏ (РїРµСЂРµРіСЂСѓР·РєР° РѕРїРµСЂР°С‚РѕСЂР° =)
+	void print(); // Р¤СѓРЅРєС†РёСЏ РІС‹РІРѕРґР° РёРЅС„РѕСЂРјР°С†РёРё РѕР± РѕР±СЉРµРєС‚Рµ
 
 	void getname();
 	void getversion();
@@ -86,12 +86,12 @@ private:
 
 template <typename a, typename b>
 void APDevelopment<a, b>::print() {
-	cout << "Название: " << name << endl;
-	cout << "Версия: " << version << endl;
-	cout << "Цена: " << cost << endl;
-	cout << "Поддерживается на платформах: " << platform << endl;
-	cout << "Поддерживаемые языки программирования: " << language << endl;
-	cout << "Фильтр: " << filter << endl;
+	cout << "РќР°Р·РІР°РЅРёРµ: " << name << endl;
+	cout << "Р’РµСЂСЃРёСЏ: " << version << endl;
+	cout << "Р¦РµРЅР°: " << cost << endl;
+	cout << "РџРѕРґРґРµСЂР¶РёРІР°РµС‚СЃСЏ РЅР° РїР»Р°С‚С„РѕСЂРјР°С…: " << platform << endl;
+	cout << "РџРѕРґРґРµСЂР¶РёРІР°РµРјС‹Рµ СЏР·С‹РєРё РїСЂРѕРіСЂР°РјРјРёСЂРѕРІР°РЅРёСЏ: " << language << endl;
+	cout << "Р¤РёР»СЊС‚СЂ: " << filter << endl;
 }
 
 template <typename a, typename b>
@@ -114,7 +114,7 @@ APDevelopment<a, b>::APDevelopment(){
 	strcpy_s(filter, 4, "No");
 }
 
-//конструктор по умолчанию
+//РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ
 template <typename a, typename b>
 ApplicationDevelopment<a, b>::ApplicationDevelopment() {
 	cost = 0;
@@ -128,7 +128,7 @@ ApplicationDevelopment<a, b>::ApplicationDevelopment() {
 	strcpy_s(language, 7, "Noname");
 }
 
-//конструктор с параметрами
+//РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ СЃ РїР°СЂР°РјРµС‚СЂР°РјРё
 template <typename a, typename b>
 ApplicationDevelopment<a, b>::ApplicationDevelopment(const a * nameVal, const a * versionVal, b costVal, const a * platformVal, const a * languageVal) {
 	name = nullptr;
@@ -136,7 +136,7 @@ ApplicationDevelopment<a, b>::ApplicationDevelopment(const a * nameVal, const a 
 	platform = nullptr;
 	language = nullptr;
 	try {
-		if (costVal < 0) throw APDExc::BelowZero("Стоимоть меньше 0");
+		if (costVal < 0) throw APDExc::BelowZero("РЎС‚РѕРёРјРѕС‚СЊ РјРµРЅСЊС€Рµ 0");
 
 		cost = costVal;
 		name = new char[strlen(nameVal) + 1];
@@ -149,12 +149,12 @@ ApplicationDevelopment<a, b>::ApplicationDevelopment(const a * nameVal, const a 
 		strcpy_s(language, strlen(languageVal) + 1, languageVal);
 	}
 	catch (APDExc::BelowZero e) {
-		cout << "Ошибка:" << e.GetError() << endl;
-		cout << "Объект не будет создан" << endl;
+		cout << "РћС€РёР±РєР°:" << e.GetError() << endl;
+		cout << "РћР±СЉРµРєС‚ РЅРµ Р±СѓРґРµС‚ СЃРѕР·РґР°РЅ" << endl;
 	}
 }
 
-//копирование
+//РєРѕРїРёСЂРѕРІР°РЅРёРµ
 template <typename a, typename b>
 ApplicationDevelopment<a, b>::ApplicationDevelopment(const ApplicationDevelopment<a, b> &val) {
 	cost = val.cost;
@@ -168,7 +168,7 @@ ApplicationDevelopment<a, b>::ApplicationDevelopment(const ApplicationDevelopmen
 	strcpy_s(language, strlen(val.language) + 1, val.language);
 }
 
-//деструктор
+//РґРµСЃС‚СЂСѓРєС‚РѕСЂ
 template <typename a, typename b>
 ApplicationDevelopment<a, b>::~ApplicationDevelopment() {
 	if (name != nullptr && version != nullptr && platform != nullptr && language != nullptr)
@@ -178,7 +178,7 @@ ApplicationDevelopment<a, b>::~ApplicationDevelopment() {
 	delete[] language;
 }
 
-//перегузка =
+//РїРµСЂРµРіСѓР·РєР° =
 template <typename a, typename b>
 void ApplicationDevelopment<a, b>::operator =(ApplicationDevelopment<a, b> &val) {
 	cost = val.cost;
@@ -192,7 +192,7 @@ void ApplicationDevelopment<a, b>::operator =(ApplicationDevelopment<a, b> &val)
 	strcpy_s(language, strlen(val.language) + 1, val.language);
 }
 
-//перегрузка +
+//РїРµСЂРµРіСЂСѓР·РєР° +
 template <typename a, typename b>
 ApplicationDevelopment<a, b> ApplicationDevelopment<a, b>::operator +(ApplicationDevelopment<a, b> val) {
 	ApplicationDevelopment created;
@@ -223,36 +223,36 @@ ApplicationDevelopment<a, b> ApplicationDevelopment<a, b>::operator +(Applicatio
 
 template <typename a, typename b>
 void ApplicationDevelopment<a, b>::print() {
-	cout << "Название: " << name << endl;
-	cout << "Версия: " << version << endl;
-	cout << "Цена: " << cost << endl;
-	cout << "Поддерживается на платформах: " << platform << endl;
-	cout << "Поддерживаемые языки программирования: " << language << endl;
+	cout << "РќР°Р·РІР°РЅРёРµ: " << name << endl;
+	cout << "Р’РµСЂСЃРёСЏ: " << version << endl;
+	cout << "Р¦РµРЅР°: " << cost << endl;
+	cout << "РџРѕРґРґРµСЂР¶РёРІР°РµС‚СЃСЏ РЅР° РїР»Р°С‚С„РѕСЂРјР°С…: " << platform << endl;
+	cout << "РџРѕРґРґРµСЂР¶РёРІР°РµРјС‹Рµ СЏР·С‹РєРё РїСЂРѕРіСЂР°РјРјРёСЂРѕРІР°РЅРёСЏ: " << language << endl;
 }
 
 //get
 template <typename a, typename b>
 void ApplicationDevelopment<a, b>::getname() {
-	cout << "Название: " << name << endl;
+	cout << "РќР°Р·РІР°РЅРёРµ: " << name << endl;
 }
 template <typename a, typename b>
 void ApplicationDevelopment<a, b>::getversion() {
-	cout << "Версия: " << version << endl;
+	cout << "Р’РµСЂСЃРёСЏ: " << version << endl;
 }
 
 template <typename a, typename b>
 void ApplicationDevelopment<a, b>::getcost() {
-	cout << "Цена: " << cost << endl;
+	cout << "Р¦РµРЅР°: " << cost << endl;
 }
 
 template <typename a, typename b>
 void ApplicationDevelopment<a, b>::getplatform() {
-	cout << "Поддерживается на платформах: " << platform << endl;
+	cout << "РџРѕРґРґРµСЂР¶РёРІР°РµС‚СЃСЏ РЅР° РїР»Р°С‚С„РѕСЂРјР°С…: " << platform << endl;
 }
 
 template <typename a, typename b>
 void ApplicationDevelopment<a, b>::getlanguage() {
-	cout << "Поддерживаемые языки программирования: " << language << endl;
+	cout << "РџРѕРґРґРµСЂР¶РёРІР°РµРјС‹Рµ СЏР·С‹РєРё РїСЂРѕРіСЂР°РјРјРёСЂРѕРІР°РЅРёСЏ: " << language << endl;
 }
 
 
@@ -272,12 +272,12 @@ void ApplicationDevelopment<a, b>::setversion(char * newversion) {
 template <typename a, typename b>
 void ApplicationDevelopment<a, b>::setcost(int newcost) {
 	try {
-		if (newcost < 0) throw APDExc::BelowZero("Стоимость - отрицательное число");
+		if (newcost < 0) throw APDExc::BelowZero("РЎС‚РѕРёРјРѕСЃС‚СЊ - РѕС‚СЂРёС†Р°С‚РµР»СЊРЅРѕРµ С‡РёСЃР»Рѕ");
 		this->cost = newcost;
 	}
 	catch (APDExc::BelowZero e) {
-		cout << "Ошибка: " << e.GetError() << endl;
-		cout << "Изменение не было выполнено" << endl;
+		cout << "РћС€РёР±РєР°: " << e.GetError() << endl;
+		cout << "РР·РјРµРЅРµРЅРёРµ РЅРµ Р±С‹Р»Рѕ РІС‹РїРѕР»РЅРµРЅРѕ" << endl;
 	}
 }
 
@@ -307,4 +307,3 @@ int main()
 	system("pause");
 	return 0;
 }
-
